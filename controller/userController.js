b6e00409e04e79266314c7fbdf9138df919f47b7
wucs <wuchangshui@chinasofti.com>
2022-09-14 22:47:21 +0800

    图片上传

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,8 @@
 const {User} = require('../model')
 const {createToken} = require('../utils/jwt')
+const fs = require('fs')
+const {promisify} = require('util')
+const rename = promisify(fs.rename)
 
 // 用户注册
 exports.register = async (req, res) => {
@@ -34,4 +37,20 @@ exports.update = async (req, res) => {
     // res.send(req.userInfo._id)
     const updateData = await User.findByIdAndUpdate(req.userInfo._id, req.body, {new: true})
     res.json(updateData)
+}
+// 上传图片
+exports.uploadImg = async (req, res) => {
+    // res.send(req.file)
+    const {originalname, filename} = req.file
+    const fileArr = originalname.split('.')
+    const fileType = fileArr[fileArr.length - 1]
+    const basePathUrl = './public/images/'
+    try {
+        await rename(`${basePathUrl}${filename}`, `${basePathUrl}${filename}.${fileType}`)
+        res.json({
+            filePath: `${filename}.${fileType}`
+        })
+    } catch (error) {
+        res.status(500).json({error})
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const userController = require('../controller/userController')
 const userValidator = require('../middleware/validator/userValidator')
 const {verifyToken} = require('../utils/jwt')
+const multer = require('multer')
+const upload = multer({dest: 'public/images'})
 
 
 // console.log(userValidator.register)
@@ -12,5 +14,6 @@ router.post('/register', userValidator.register,userController.register)
       .post('/login',userValidator.login, userController.login)
       .get('/list', verifyToken, userController.list)
       .put('/', verifyToken, userValidator.update,userController.update)
+      .post('/uploadImg', verifyToken, upload.single('avatar'),userController.uploadImg)
 
 module.exports = router;
